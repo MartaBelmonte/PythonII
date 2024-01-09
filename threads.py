46f185f9ec6486f52c74689281aae90661074f2c
MartaBelmonte <martabelmonts5@gmail.com>
2024-01-09 23:10:12 +0100

    comments

--- threads.py
+++ threads.py
@@ -3,12 +3,10 @@ import time
 
 
 def print_message_without_newline(message):
-    # Función auxiliar para imprimir un mensaje sin salto de línea
     print(message, end='', flush=True)
 
 
 def thread_task(message, delay):
-    # Función que ejecuta cada thread
     time.sleep(delay)
     print_message_without_newline(message)
 
@@ -16,7 +14,7 @@ def thread_task(message, delay):
 def concurrency_with_coffee():
     print("- I'm tired, Bob! ", end='', flush=True)
 
-    # Crear e iniciar threads uno por uno
+
     thread1 = threading.Thread(
         target=thread_task, args=("are made of coffee? ", 0))
     thread2 = threading.Thread(target=thread_task, args=("Do you know ", 1))
@@ -25,14 +23,14 @@ def concurrency_with_coffee():
         target=thread_task, args=(" that our bodies ", 3))
     thread5 = threading.Thread(target=thread_task, args=("-Try drinking ", 4))
 
-    # Iniciar los threads
+    # Iniciar threads
     thread1.start()
     thread2.start()
     thread3.start()
     thread4.start()
     thread5.start()
 
-    # Esperar a que todos los threads terminen
+    # Esperar threads terminados
     thread1.join()
     thread2.join()
     thread3.join()
